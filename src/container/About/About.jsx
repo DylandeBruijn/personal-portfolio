9eb345414e9270d9a8441ed2d3af3caa982b74d5
Dylan de Bruijn <dylandebruyn1993@gmail.com>
2023-02-04 23:50:24 +0100

    Add MotionWrap HOC

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import "./About.scss";
 import { urlFor, client } from "../../client";
 
@@ -20,7 +20,7 @@ const About = () => {
       </h2>
 
       <div className="app__profiles">
-        {abouts.map((about, index) => (
+        {abouts?.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
@@ -42,4 +42,8 @@ const About = () => {
   );
 };
 
-export default AppWrap(About, 'about')
+export default AppWrap(
+  MotionWrap(About, "app__about"),
+  "about",
+  "app__whitebg"
+);
